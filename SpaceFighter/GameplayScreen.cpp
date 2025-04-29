ec92d5c0936397b6fdbe979808ac8fc503a35fee
Garza <300079729@fvtc.edu>
2025-04-29 12:04:36 -0500

    added level03 cpp + header files

--- SpaceFighter/GameplayScreen.cpp
+++ SpaceFighter/GameplayScreen.cpp
@@ -3,6 +3,8 @@
 #include "MainMenuScreen.h"
 #include "Level.h"
 #include "Level01.h"
+#include "Level02.h"
+#include "Level03.h"
 
 GameplayScreen::GameplayScreen(const int levelIndex)
 	: m_levelIndex(levelIndex)
@@ -28,6 +30,8 @@ void GameplayScreen::LoadLevel(const int levelIndex)
 	switch (levelIndex)
 	{
 	case 0: m_pLevel = new Level01(); break;
+	/*case 1: m_pLevel = new Level02(); break;
+	case 2: m_pLevel = new Level03(); break;*/
 	}
 
 	m_pLevel->SetGameplayScreen(this);
--- SpaceFighter/Level03.cpp
+++ SpaceFighter/Level03.cpp
@@ -1,10 +1,9 @@
 
-
-#include "Level02.h"
+#include "Level03.h"
+// for boss ship #include 
 #include "BioEnemyShip.h"
 
-
-void Level02::LoadContent(ResourceManager& resourceManager)
+void Level03::LoadContent(ResourceManager& resourceManager)
 {
 	// Setup enemy ships
 	Texture* pTexture = resourceManager.Load<Texture>("Textures\\BioEnemyShip.png");
@@ -18,7 +17,6 @@ void Level02::LoadContent(ResourceManager& resourceManager)
 		0.3, 0.25, 0.35, 0.2, 0.4,
 		0.7, 0.75, 0.65, 0.8, 0.6,
 		0.5, 0.4, 0.6, 0.45, 0.55, .6
-
 	};
 
 	double delays[COUNT] =
@@ -45,9 +43,5 @@ void Level02::LoadContent(ResourceManager& resourceManager)
 		AddGameObject(pEnemy);
 	}
 
-	SetBackground(resourceManager.Load<Texture>("Textures\\SpaceBackground03.png"));
-
-
 	Level::LoadContent(resourceManager);
 }
-
